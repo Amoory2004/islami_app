860ab36dbd6612c960f3ecee1349a05deb9f3d67
omar khaled <omarkhaled10102004@gmail.com>
2025-07-06 23:59:21 +0300

    initialize project

---
+++ lib/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
