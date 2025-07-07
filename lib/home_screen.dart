057ef238b4fa12fffb8b842b02c9e2822d15abe9
omar khaled <omarkhaled10102004@gmail.com>
2025-07-07 16:26:40 +0300

    add home header & tabs background

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:islami/nav_bar_selected_icon.dart';
 import 'package:islami/nav_bar_unselected_icon.dart';
+import 'package:islami/tabs/hadeth_tab.dart';
+import 'package:islami/tabs/quran_tab.dart';
+import 'package:islami/tabs/radio_tab.dart';
+import 'package:islami/tabs/sebha_tab.dart';
+import 'package:islami/tabs/time_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   static const routeName = '/';
@@ -12,9 +17,45 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int currentIndex = 0;
 
+  List<Widget> tabs = [
+    QuranTab(),
+    HadethTab(),
+    SebhaTab(),
+    RadioTab(),
+    TimeTab(),
+  ];
+
+  List<String> backgroundNames = [
+    'quran_background',
+    'hadeth_background',
+    'sebha_background',
+    'radio_background',
+    'time_background',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/${backgroundNames[currentIndex]}.png',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/header.png',
+              height: MediaQuery.sizeOf(context).height * .17,
+            ),
+            tabs[currentIndex],
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
         onTap: (index) {
